fa2e7f4b43e47e6fe580b105d1dca2c4afdd6c91
danielazieba <danielaazieba@gmail.com>
2019-05-20 20:57:10 -0400

    Form now allows for selection of states to get list of campgrounds

--- npsflaskapp/routes.py
+++ npsflaskapp/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template, request
 from npsflaskapp import app
+
+import subprocess
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Yeet!"
+    return render_template('index.html')
+
+@app.route('/alerts')
+def alerts():
+    subprocess_output = subprocess.check_output(['curl -X GET "https://developer.nps.gov/api/v1/alerts?stateCode=KY&api_key=FCzzTNkAX72099q1ja44eHVTYI27yOos2clMXKkT" -H "accept: application/json"'], shell=True)
+    return subprocess_output
+
+@app.route('/campgrounds', methods=['GET','POST'])
+def campgrounds():
+    selection = request.args.getlist('requestedStates')
+    subprocess_output = subprocess.check_output(['curl -X GET "https://developer.nps.gov/api/v1/campgrounds?stateCode=KY%2CAL%2CFL&api_key=FCzzTNkAX72099q1ja44eHVTYI27yOos2clMXKkT" -H "accept: application/json"'], shell=True)
+    return render_template('campgrounds.html', campground_list = subprocess_output)
