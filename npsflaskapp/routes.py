3b59fd776a75aa5cc69f5c115808fa68543bfd90
Daniela <danielaazieba@gmail.com>
2019-07-02 22:09:45 -0400

    Removed method no longer in use

--- npsflaskapp/routes.py
+++ npsflaskapp/routes.py
@@ -15,18 +15,6 @@ def index():
         data = json.load(json_file)
     return render_template('index.html', states=data)
 
-@app.route('/campgrounds', methods=['GET','POST'])
-def campgrounds():
-    selection = request.args.getlist('requestedStates')
-    subprocess_output = subprocess.check_output(['curl -X GET "https://developer.nps.gov/api/v1/campgrounds?stateCode=KY%2CAL%2CFL&api_key=FCzzTNkAX72099q1ja44eHVTYI27yOos2clMXKkT" -H "accept: application/json"'], shell=True)
-    campground_str = subprocess_output.decode('utf-8')
-    campground_json = json.loads(campground_str)
-    campground_names = []
-    for campground in campground_json['data']:
-        campground_names.append(campground['name'])
-    return render_template('campgrounds.html', campground_list = campground_names)
-
-
 @app.route('/parks', methods=['GET','POST'])
 def parks():
     keyword = request.args.get('keyword')
